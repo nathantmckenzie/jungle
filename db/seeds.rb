52a142aac34a58c5e68a7dc39e1f53bb702314f9
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-01-05 20:49:54 +0000

    Sold Out Badge

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ cat1.products.create!({
   name:  'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel1.jpg'),
-  quantity: 10,
+  quantity: 0,
   price: 64.99
 })
 
@@ -63,7 +63,7 @@ cat1.products.create!({
   name:  'Hipster Socks',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel4.jpg'),
-  quantity: 8,
+  quantity: 0,
   price: 25.00
 })
 
